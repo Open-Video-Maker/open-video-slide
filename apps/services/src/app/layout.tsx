2d15fc2885511c828d04981be35d7af67510ee70
Zhi.Huang <zh.impact@outlook.com>
2023-11-01 12:02:20 +0800

    chore: add mantine theme

--- apps/services/src/app/layout.tsx
+++ apps/services/src/app/layout.tsx
@@ -4,6 +4,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 
+import { theme } from "../theme";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -22,7 +24,7 @@ export default function RootLayout({
         <ColorSchemeScript />
       </head>
       <body className={inter.className}>
-        <MantineProvider>{children}</MantineProvider>
+        <MantineProvider theme={theme}>{children}</MantineProvider>
       </body>
     </html>
   );
